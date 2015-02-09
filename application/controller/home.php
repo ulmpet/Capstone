9caacb5db5cc71e60ae08e49a1068e683d59a883
Sam B <sam.butler601@gmail.com>
2015-02-08 20:17:00 -0600

    button labels and login funtions

--- application/controller/home.php
+++ application/controller/home.php
@@ -27,19 +27,23 @@ class Home extends Controller
      * This method handles what happens when you move to http://yourproject/home/exampleone
      * The camelCase writing is just for better readability. The method name is case-insensitive.
      */
-    public function exampleOne()
+    public function fileupload()
     {
-        // load views
+        //Check to see the the SuperGlobal Variable $_FILES has data
         if(isset($_FILES['userfile']['name'])){
+            //CHeck for file upload error resulting in null file
             if($_FILES['userfile']['name']!=null){
-            echo print_r($_FILES);
+            //open the uploaded file for reading
             $file = fopen($_FILES['userfile']['tmp_name'], 'r');
+            //while not end of file loop get line as an array
             while(!feof($file)){
-                echo fgets($file) . "<br>";
+                //print out our array for viewing pleasure.
+                echo print_r(fgetcsv($file,0,"\t")) . "<br>";
             }
             fclose($file);
             }
         }
+        // load views
         require APP . 'view/_templates/header.php';
         require APP . 'view/home/example_one.php';
         require APP . 'view/_templates/footer.php';
@@ -50,9 +54,19 @@ class Home extends Controller
      * This method handles what happens when you move to http://yourproject/home/exampletwo
      * The camelCase writing is just for better readability. The method name is case-insensitive.
      */
-    public function exampleTwo()
+    public function login()
     {
+        //is the http request contains information from a feild called VARIABLEAMEFOREMAIL
+        if(isset($_REQUEST['VARIABLENAMEFOREMAIL'])){
+        $user = $this->loadModel('user');
+        $userEmail = $_REQUEST['VARIABLENAMEFOREMAIL'];
+        $userPassword = $_REQUEST['VARIABLENAMEFORPASSOWRD'];
         // load views
+        if($user->checkLogin($userEmail,$userPassword)){
+            session_start();
+            header('location: /information');
+        }
+    }
         require APP . 'view/_templates/header.php';
         require APP . 'view/home/example_two.php';
         require APP . 'view/_templates/footer.php';
