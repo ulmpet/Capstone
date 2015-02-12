f4c9b0b827be22b38d528247a3583a32debd471c
Sam B <sam.butler601@gmail.com>
2015-02-11 23:05:10 -0600

    file upload hotfix

--- application/view/home/example_one.php
+++ application/view/home/example_one.php
@@ -1,4 +1,11 @@
 <div class="container">
     <h2>You are in the View: application/view/home/example_one.php (everything in the box comes from this file)</h2>
-    <p>In a real application this could be a normal page.</p>
+    <!-- The data encoding type, enctype, MUST be specified as below -->
+<form enctype="multipart/form-data" action="" method="POST">
+    <!-- MAX_FILE_SIZE must precede the file input field -->
+    <input type="hidden" name="MAX_FILE_SIZE" value="56320000" />
+    <!-- Name of input element determines name in $_FILES array -->
+    Send this file: <input name="userfile" type="file" />
+    <input type="submit" value="Send File" />
+</form>
 </div>
