6be1f8f42e38fc09f8d8fce923cb344b2eafbfba
gautremf <gautremf@gmail.com>
2015-02-21 23:38:02 -0600

    added news
    Home goes to news

--- application/view/_templates/nav.php
+++ application/view/_templates/nav.php
@@ -1,6 +1,6 @@
     <!-- navigation -->
     <div class="navigation">
-        <a href="<?php echo URL; ?>">Home</a>
+        <a href="<?php echo URL; ?>news">Home</a>
         <a href="<?php echo URL; ?>account">Account</a>
         <a href="<?php echo URL; ?>phagetool">Phage Data</a>
         <a href="<?php echo URL; ?>dashboard">Dashboard</a>
