c7796e55e1f1fa1cde435cc331918bd36fefaf7b
Sam B <sam.butler601@gmail.com>
2015-02-22 00:17:27 -0600

    and now the link works AND responds to authLevel

--- application/view/_templates/nav.php
+++ application/view/_templates/nav.php
@@ -3,6 +3,7 @@
         <a href="<?php echo URL; ?>news">Home</a>
         <a href="<?php echo URL; ?>account">Account</a>
         <a href="<?php echo URL; ?>phagetool">Phage Data</a>
-        <?php if($this->getAuth() >= 1){ echo '<a href="<?php echo URL; ?>dashboard">Dashboard</a>';} ?>
+        
+        <a href="<?php echo URL; ?>dashboard" <?php if($this->getAuth() >= 1){echo 'style="display:none"';}?> >Dashboard</a>
         <a href="<?php echo URL; ?>home/logout">Log Out</a>
     </div>
