9e71c17bb8a55f0b0307e66b40c963367c5e72c7
gautremf <gautremf@gmail.com>
2015-02-21 23:47:31 -0600

    Fixes
    Added some views for account and phagetool, also OCD'd the dashboard

---
+++ application/view/account/account.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <h2>Account</h2>
+    <pre>
+     <u><bold>Features to be included:</bold></u>
+       -Phage filtering mechanisms (cluster, sub-cluster, genus, …)
+       -Deactivation of accounts (not for admins, only super-admin can deactivate Admin.)
+
+ </pre>
+</div>
