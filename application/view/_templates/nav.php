6f29289f23d55173c037a07cb70d1863267ba377
gautremf <gautremf@gmail.com>
2015-02-21 23:05:26 -0600

    Created dashboard.php
    Also added Dashboard button to Nav.php

--- application/view/_templates/nav.php
+++ application/view/_templates/nav.php
@@ -1,7 +1,8 @@
     <!-- navigation -->
     <div class="navigation">
-        <a href="<?php echo URL; ?>">home</a>
-        <a href="<?php echo URL; ?>home/fileupload">File Upload Sample</a>
-        <a href="<?php echo URL; ?>home/error">Login Sample</a>
-        <a href="<?php echo URL; ?>songs">songs</a>
+        <a href="<?php echo URL; ?>">Home</a>
+        <a href="<?php echo URL; ?>account">Account</a>
+        <a href="<?php echo URL; ?>phagetool">Phage Data</a>
+        <a href="<?php echo URL; ?>dashboard">Dashboard</a>
+        <a href="<?php echo URL; ?>home/logout">Log Out</a>
     </div>
