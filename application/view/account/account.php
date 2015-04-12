b0419cbcf1fb76454e9aa7dbae69fd8827be6476
gautremf <gautremf@gmail.com>
2015-04-12 17:12:13 -0500

    Password change
    Added the forms and boxes for users to change their password.

--- application/view/account/account.php
+++ application/view/account/account.php
@@ -1,9 +1,38 @@
 <div class="container">
-    <h2>Account</h2>
-    <pre>
-     <u><bold>Features to be included:</bold></u>
-       -Phage filtering mechanisms (cluster, sub-cluster, genus, …)
-       -Deactivation of accounts (not for admins, only super-admin can deactivate Admin.)
+    Account
 
- </pre>
+    <div>
+        <form method = "POST" action = "">
+            <br>
+            Click here if you would like to deactivate your account.
+            <br>
+            <input name="Deactivate" type="submit" value="Deactivate"/> 
+        </form>
+    </div>
+
+    <div>
+        <h1>Change Password</h1>
+        <form method="POST" action="">
+    <table>
+        <tr>
+            <td>Enter your existing password:</td>
+            <td><input type="password" size="16" name="password"></td>
+        </tr>
+        <tr>
+            <td>Enter your new password:</td>
+            <td><input type="password" size="16" name="newpassword"></td>
+        </tr>
+        <tr>
+            <td>Re-enter your new password:</td>
+            <td><input type="password" size="16" name="confirmnewpassword"></td>
+        </tr>
+    </table>
+            <p><input type= "submit" value="Change Password"> 
+        </form>
+    </div>
+    <p>
+        <u><bold>Features to be included:</bold></u></br>
+        -Phage filtering mechanisms (cluster, sub-cluster, genus, …)</br>
+        -Deactivation of accounts (not for admins, only super-admin can deactivate Admin.)</br>
+    </p>
 </div>
