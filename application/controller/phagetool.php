4363d057d6e2730c955f0a1a550bd2f75e6c3aed
Sam B <sam.butler601@gmail.com>
2015-04-21 16:45:24 -0500

    write infile data
    alsdfjlakdsjf

--- application/controller/phagetool.php
+++ application/controller/phagetool.php
@@ -186,8 +186,17 @@ class PhageTool extends Controller
 
                         //Write data to fileObject
                         $filename = 'Infile_' . date('U');
-                        if(fopen(PHYLIP_DATA.$filename.".txt", 'w')){
-
+                        if($file = fopen(PHYLIP_DATA.$filename.".txt", 'w')){
+                            fwrite($file, count($outputNames) . " " . strlen($cutBucketStrings[0]) ."\n");
+                            foreach ($outputNames as $key => $value) {
+                                fwrite($file, $value)
+                                if(strlen($value) < 10 ){
+                                    $whitespacearray = array_fill(0, 10-strlen($value), " ");
+                                    $whiteSpaceString = implode("", $whitespacearray);
+                                    fwrite($file, $whiteSpaceString);
+                                }
+                                fwrite($file, $cutBucketStrings[$key]);
+                            }
                         }else{
                             echo "Filed to open " . $filename . " For Write Operation";
                         }
