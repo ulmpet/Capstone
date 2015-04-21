20adda580146718dd3cabac90fdd7e07b50ce884
Michael Bruscato <theformula318@gmail.com>
2015-04-21 16:09:16 -0500

    Sign-up form tweak
    Removed city, changed orientation

--- application/view/home/signup.php
+++ application/view/home/signup.php
@@ -1,13 +1,11 @@
 <div class="container">
 	<p><?php echo $this->message ?>
-	<form method='post' action=''></br>
-	<label>Your email<input type="text" name="email"/></label></br>
-	<label>Enter a password<input type="password" name="pass"/></label></br>
-	<label>Re-enter your password<input type="password" name="passconfirm"/></label></br>
-	<label>Your organization or institution<input type="text" name="organization"/></label></br>
-	<label>Your City<input type="text" name="city"/></label></br>
-	<p>By providing us with this extra information, you are contributing to the improvement of these tools.</p>
+	<form method='post' action=''>
+	<label>Your email:</br><input type="text" name="email"/></label></br></br>
+	<label>Enter a password:</br><input type="password" name="pass"/></label></br></br>
+	<label>Re-enter your password:</br><input type="password" name="passconfirm"/></label></br></br>
+	<label>*Your organization/institution:</br><input type="text" name="organization"/></label></br>
+	<p>&nbsp&nbsp&nbsp*By providing us with this extra information, you are contributing to the improvement of these tools.</p>
 	<input type="submit"/>
 	</form>
-
 </div>
