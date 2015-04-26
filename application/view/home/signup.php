84ad751913da8a042a42db390a3693d37f61a270
Michael Bruscato <theformula318@gmail.com>
2015-04-26 00:49:20 -0500

    reCaptcha verification
    Start of implementation of Google reCaptcha for user signup validation
    to prevent spam registration attempts. This doesn't seem to be working
    properly. Can I get a review?

--- application/view/home/signup.php
+++ application/view/home/signup.php
@@ -1,34 +1,35 @@
 <div class="container">
 	<h3>Please enter all required information.</h3>
-	<form method='post' action='https://www.google.com/recaptcha/api/siteverify'><br>
+	<form method='post' action='verify.php'><br>
 		<table>
-
 			<tr>
 				<td>Your email</td>
 				<td><input type="text" name="email"/></td>
 				<?php if(isset($_SESSION['goodemail'])){ echo "<td class = 'ui-state-highlight'> ".$_SESSION['goodemail']." </td>";}else{echo "<td> </td>";} ?>
 				<?php if(isset($_SESSION['bademail'])){ echo "<td class = 'ui-state-error'> ".$_SESSION['bademail']." </td>";}else{echo "<td> </td>";} ?>
 			</tr>
-			<td> </td>
+			<td></td>
 			<tr>
 				<td>Enter a password</td>
 				<td><input type="password" name="pass"/></td>
-
 				<td>Re-enter your password</td>
 				<td><input type="password" name="passconfirm"/></td>
 				<?php if(isset($_SESSION['passmessage'])){ echo "<td class = 'ui-state-error'> ".$_SESSION['passmessage']." </td>";}else{echo "<td> </td>";} ?>
 			</tr>
 		</table>
-	<br>
+	<br>	
 	
-	<div class="g-recaptcha" data-sitekey="6Ldg5AUTAAAAAGgnCFx4b7HPkuhYnbzjbLxuwIDt"></div>
-
-	<input type="submit"/>
+	<div class="g-recaptcha" data-sitekey="6Ldg5AUTAAAAAGgnCFx4b7HPkuhYnbzjbLxuwIDt" data-theme="dark" align="center"></div>
+	
+	<input type="submit" value="Submit"/>
 	</form>
-
 </div>
-<?php 
+
+
+<?php
+
     unset($_SESSION['goodemail']);
     unset($_SESSION['bademail']);
     unset($_SESSION['passmessage']);
+
 ?>
